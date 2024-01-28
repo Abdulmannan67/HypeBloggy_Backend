62c27f71a7e3f86781556cd518774ea9b1b8e52c
Abdul Mannan <am2595171@gmail.com>
2024-01-28 15:52:16 +0530

    Files add

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const connectToMongo = require('./db')
+var cors = require('cors')
+connectToMongo();
+
+const app = express()
+const port = process.env.PORT|| 8000;
+
+//Now, we are getting an error as the access to fetch the API from the https://localhost:3000 has been blocked by the CORS policy. 
+
+app.use(cors())
+
+app.use(express.json())
+app.use('/api/auth', require("./routes/auth"));
+app.use('/api/notes', require("./routes/notes"));
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
